dae533ab0d2c1da45bb40853a6bafd0ee26f1c45
nani <naninarendra2000@mail.com>
2024-05-22 23:37:08 +0530

    ep7

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,7 @@ import Smmer from "./Smmer";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 const Body = () => {
   const [listreslist, setListreslist] = useState([]);
@@ -20,8 +21,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4387245&lng=78.4453532&collection=83646&tags=layout_CCS_SouthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
     );
     const json = await data.json();
-    const fetchedRestaurants =
-      json.data.cards.slice(3);
+    const fetchedRestaurants = json.data.cards.slice(3);
     console.log(fetchedRestaurants);
     setListreslist(fetchedRestaurants);
     setFilterserch(fetchedRestaurants);
@@ -32,59 +32,63 @@ const Body = () => {
   ) : (
     <div className="res-container">
       <div className="fil-container">
-      <div className="btn-flt">
-        <TextField
-          variant="outlined"
-          placeholder="Search..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-         
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        
-        <Button
-           variant="contained"
-          onClick={() => {
-            const filter_serch = listreslist.filter((res) => {
-              return res.card.card.info.name.toLowerCase().includes(search.toLowerCase());
-            });
-            setFilterserch(filter_serch);
-          }}
-        >
-          search
-        </Button>
-        
-        
+        <div className="btn-flt">
+          <TextField
+            variant="outlined"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button
+            variant="contained"
+            onClick={() => {
+              const filter_serch = listreslist.filter((res) => {
+                return res.card.card.info.name
+                  .toLowerCase()
+                  .includes(search.toLowerCase());
+              });
+              console.log(filter_serch);
+              setFilterserch(filter_serch);
+            }}
+          >
+            search
+          </Button>
+        </div>
+        <div className="topreted">
+          <Button
+            variant="contained"
+            onClick={() => {
+              const filterres_list = listreslist.filter((res) => {
+                return res.card.card.info.avgRating > 4;
+              });
+
+              setFilterserch(filterres_list);
+              console.log("not");
+            }}
+          >
+            Topreted
+          </Button>
+        </div>
       </div>
-      <div className="topreted">
-      <Button
-          variant="contained"
-          onClick={() => {
-            const filterres_list = listreslist.filter((res) => {
-              return res.card.card.info.avgRating > 4;
-            });
 
-            setFilterserch(filterres_list);
-            console.log("not")
-          }}
-        >
-          Topreted
-        </Button>
-
-      </div>
-
-      </div>
-     
-
-      <div className="res_list"> 
+      <div className="res_list">
         {filterserch.map((res) => {
-          return <Res_card key={res.card.card.info?.id} {...res.card.card.info} />;
+          return (
+            <Link
+              key={res.card.card.info?.id}
+              to={/restarent/ + res.card.card.info?.id}
+            >
+              <Res_card {...res.card.card.info} />
+            </Link>
+          );
         })}
       </div>
     </div>
