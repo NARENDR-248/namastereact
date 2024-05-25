4a1aff83d97db0f0f8407ddcdc5fb29e9064f5dd
nani <naninarendra2000@mail.com>
2024-05-25 20:38:30 +0530

    ep-9

--- src/App.js
+++ src/App.js
@@ -1,64 +1,44 @@
 import React, { Suspense, lazy } from "react";
 import * as ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route ,Outlet} from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter,RouterProvider ,Outlet} from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
-import { RestaurantMenu } from "@mui/icons-material";
 import RestaurentMenu from "./components/RestaurentMenu";
-// import Grocery from "./components/Grocery";
-const Grocery=lazy(()=>import("./components/Grocery"))
 
+const Grocery = lazy(() => import("./components/Grocery"));
 
+const AppLayout = () => (
+  <div className="container">
+    <Header />
+    <Outlet />
+  </div>
+);
 
-
-
-const AppLayout = () => {
-  
-  return (
-    <div className="container">
-      <Header />
-      <Outlet />
-      
-    </div>
-  );
-};
-
-const approuter=createBrowserRouter([
-  {
-    path:"/",
-    element:<AppLayout/>,
-    children:[
-      {
-        path:"/",
-        element:<Body />
-      },
-      {
-        path:"/about",
-        element:<About />
-      },
-      {
-        path:"/contact",
-        element:<Contact />
-      },
-      {
-        path:"/grocery",
-        element:<Suspense fallback={<h1>this loading</h1>}>< Grocery /></Suspense>
-      },
-      {
-        path:"/restarent/:resId",
-        element:<RestaurentMenu />
-      }
-
-
-    ],
-    errorElement:<Error/>
-  }
- 
-])
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Body />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route
+          path="grocery"
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <Grocery />
+            </Suspense>
+          }
+        />
+        <Route path="restarent/:resId" element={<RestaurentMenu />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </Router>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={approuter} />);
+root.render(<App />);
